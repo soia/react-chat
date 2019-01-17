392c0f7ba377409f1cf4a92a78ea6e05c5bc415f
soia <nikita.soya@gmail.com>
2019-01-18 01:28:34 +0200

    start make material ui layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,78 @@
-import React, { Component } from "react";
-import Welcome from "./Welcome";
-import Clock from "./Clock";
-import List from "./List";
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Search from './Components/Search';
 
-class App extends Component {
-  render() {
+const styles = theme => ({
+    root: {
+        display: "flex"
+    },
+    appBar: {
+        width: `calc(100% - 320px)`,
+        marginLeft: 320,
+    
+    },
+    drawer: {
+        width: 320,
+        flexShrink: 0
+    },
+    drawerPaper: {
+        width: 320
+    },
+    toolbar: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing.unit * 3
+    }
+});
+
+function PermanentDrawerLeft(props) {
+    const { classes } = props;
+
     return (
-      <div>
-        <h1>Chat</h1>
-      </div>
+        <div className={classes.root}>
+            <AppBar className={classes.appBar}>
+                <Toolbar>
+                    <Typography variant="h6" color="inherit" noWrap>
+                        React chat by Nikita Soia
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                classes={{paper: classes.drawerPaper}}
+            >
+
+                <Search />
+
+            </Drawer>
+
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                <Typography paragraph>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+                    dolor purus non enim praesent elementum facilisis leo vel. Risus at
+                    ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+                    quisque non tellus. Convallis convallis tellus id interdum velit
+                    laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+                    adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+                    integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+                    eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+                    quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+                    vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+                    lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+                    faucibus et molestie ac.
+                </Typography>
+            </main>
+        </div>
     );
-  }
 }
 
-export default App;
+export default withStyles(styles)(PermanentDrawerLeft);
---
+++ src/Components/Search.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: '100%',
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
+class TextFields extends React.Component {
+
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <form className={classes.container} noValidate autoComplete="off">
+        <TextField
+          id="standard-search"
+          label="Search field"
+          type="search"
+          className={classes.textField}
+          margin="normal"
+        />
+      </form>
+    );
+  }
+}
+
+
+export default withStyles(styles)(TextFields);
